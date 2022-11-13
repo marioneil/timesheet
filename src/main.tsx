6fda9c63c8635fbcd9d48ab9672e57d8e45bed8d
Jay Patel <jpate34@LLMHX4599V5K.ngco.com>
2022-11-13 12:50:55 -0500

    working

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import './scss/custom.scss'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./scss/App.scss";
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
