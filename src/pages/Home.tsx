371fab47215475b8659a963634c7540e41d3bd41
marioneil <marioneilfernandes@gmail.com>
2023-02-01 20:32:46 -0500

    Home Screen refresh on delete

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,20 @@
 import { useEffect, useState } from "react";
 import { TaskCard } from "../components/TaskCard";
+import MyAlert, { AlertType } from "../components/CustomAlert";
 
 function Home() {
   const [toDo, setToDo] = useState<any[]>([]);
+  //var [messageClassName, setMessageClassName] = useState<string>("");
+  var [messageClassName, setMessageClassName] = useState<AlertType>("Info");
+  var [message, setMessage] = useState("");
+  var [updateCount, setUpdateCount] = useState(0);
+
+  const displayMessage = (message: string, type: AlertType) => {
+    setMessage(message);
+    setMessageClassName(type);
+    setUpdateCount((count) => count + 1);
+  };
+
   const fetchToDo = async () => {
     const result = await fetch("http://localhost:5000");
     const data = await result.json();
@@ -10,26 +22,33 @@ function Home() {
   };
 
   useEffect(() => {
+    console.log("in Home useEffect");
     fetchToDo();
-  }, []);
+  }, [message, updateCount]);
 
   return (
-    <div className="m-3 p-3 flex flex-wrap justify-center border-4 bg-white dark:bg-slate-800">
-      {toDo.map(
-        (x) => (
-          //{
-          //  return (
-          <TaskCard
-            key={x.id}
-            id={x.id}
-            timestampCreated={x.timestampCreated}
-            title={x.title}
-          />
-        )
+    <div>
+      <div className="flex">
+        <MyAlert message={message} type={messageClassName}></MyAlert>
+      </div>
+      <div className="m-3 flex flex-wrap justify-center border-4 bg-white p-3 dark:bg-slate-800">
+        {toDo.map(
+          (x) => (
+            //{
+            //  return (
+            <TaskCard
+              key={x.id}
+              id={x.id}
+              timestampCreated={x.timestampCreated}
+              title={x.title}
+              displayMessage={displayMessage}
+            />
+          )
 
-        //);
-        //}
-      )}
+          //);
+          //}
+        )}
+      </div>
     </div>
   );
 }
